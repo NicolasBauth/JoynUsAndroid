751179d5fc884e8da50eab1e7e4001a965c75564
NicolasBauth <nicolas.bauthier.01@student.henallux.be>
2017-11-24 15:07:47 +0100

    continuation

---
+++ Joynus/app/src/main/java/com/example/nicol/joynus/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.nicol.joynus;
+
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
